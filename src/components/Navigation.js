df8be48de504c99d5f9cfdd0e56ed9d6b6bfd72f
varun mara <varungowde@gmail.com>
2019-10-06 23:36:06 -0500

    added link and route

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,21 @@
-import React from 'react';
-
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import Contact from "./Contact";
+import About from "./About";
+import Home from "./Home";
 const Navigation = () => {
   return (
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
-        <div>
-          <a href="">Home</a>
-        </div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
-        </div>
+        <Link to="/"> Home </Link>
+        <Link to="/contact"> Contact </Link>
+        <Link to="/about"> About </Link>
+        <Route exact path="/" component={Home} />
+
+        <Route path="/about" component={About} />
+
+        <Route path="/contact" component={Contact} />
       </div>
     </div>
   );
